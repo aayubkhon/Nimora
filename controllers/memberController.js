1e0ff46551ae9931e1886b17af19a6dcf820081e
aayubxon701 <aayubxon701@gmail.com>
2025-10-13 01:02:45 +0900

    feat:login with createToken start

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,5 +1,8 @@
+const assert = require("assert");
 const Member = require("../models/Member");
 let memberController = module.exports;
+const jwt = require("jsonwebtoken");
+const Definer = require("../lib/mistake");
 
 memberController.signup = async (req, res) => {
   try {
@@ -19,6 +22,11 @@ memberController.login = async (req, res) => {
     const data = req.body;
     const member = new Member();
     result = await member.loginData(data);
+    console.log("result:", result);
+
+    const token = memberController.createToken(result);
+    console.log("token::", token);
+
     res.json({ state: "success", data: result });
   } catch (err) {
     console.log(`ERROR, cont/login,${err.message}`);
@@ -29,4 +37,21 @@ memberController.login = async (req, res) => {
 memberController.logout = (req, res) => {
   console.log("GET cont.logout");
   res.send("logout page");
+};
+
+memberController.createToken = (result) => {
+  try {
+    const upload_data = {
+      _id: result.id,
+      mb_nick: result.mb_nick,
+      mb_type: result.mb_type,
+    };
+    const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, {
+      expiresIn: "7d",
+    });
+    assert.ok(token, Definer.auth_err4);
+    return token;
+  } catch (err) {
+    throw err;
+  }
 };
