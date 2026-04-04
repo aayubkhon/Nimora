906e74c89b0fa729b23e1a005b3256b227140db7
aayubxon701 <aayubxon701@gmail.com>
2026-04-04 22:22:02 +0900

    feat: proccess.config created fork

--- process.config.js
+++ process.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "NIMORA",
     script: "./server.js",
-    watch: true,
+    watch: false,        
     env_production: {
       NODE_ENV: "production"
     },
@@ -10,6 +10,24 @@ module.exports = {
       NODE_ENV: "development"
     },
     instances: 1,
-    exec_mode: "cluster"
+    exec_mode: "fork"
   }]
 }
+
+
+
+// module.exports = {
+//   apps: [{
+//     name: "NIMORA",
+//     script: "./server.js",
+//     watch: true,
+//     env_production: {
+//       NODE_ENV: "production"
+//     },
+//     env_development: {
+//       NODE_ENV: "development"
+//     },
+//     instances: 1,
+//     exec_mode: "cluster"
+//   }]
+// }
